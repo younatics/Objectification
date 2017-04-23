675de0485ed56b463b341aebc9b02700bbdb907f
Seungyoun Yi <younatics@gmail.com>
2017-04-23 22:20:40 +0900

    Initial commit

---
+++ Objectification/Objectification.h
@@ -0,0 +1,19 @@
+//
+//  Objectification.h
+//  Objectification
+//
+//  Created by YiSeungyoun on 2017. 4. 23..
+//  Copyright © 2017년 SeungyounYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Objectification.
+FOUNDATION_EXPORT double ObjectificationVersionNumber;
+
+//! Project version string for Objectification.
+FOUNDATION_EXPORT const unsigned char ObjectificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Objectification/PublicHeader.h>
+
+
